4a74fa95fbd113456f86345155744013c49863ba
BenTenmann <benji.tenmann@me.com>
2023-05-21 12:21:52 +0100

    refactor(app.py): remove unused _DEBUG variable feat(app.py): add SearchResult dataclass to represent search results feat(app.py): add display_search_results function to display search results in a grid of videos

--- video_semantic_search/app.py
+++ video_semantic_search/app.py
@@ -1,12 +1,11 @@
 import numpy as np
 import os
+from dataclasses import dataclass
 from typing import Final
 
 import faiss
 import pandas as pd
 import streamlit as st
-
-_DEBUG = True
 
 
 class SemanticSearcher:
@@ -19,33 +18,84 @@ class SemanticSearcher:
         # TODO: self.embedder = load_embedder()
         self.embedder = lambda _: np.random.rand(1, len(dim_columns))
 
-    def search(self, query: str) -> pd.DataFrame:
+    def search(self, query: str) -> list["SearchResult"]:
         v = self.embedder(query)
         _, I = self.index.search(v, 10)
-        return self.metadata.iloc[I[0]]
+        return [
+            SearchResult(id=row["id"], second=row["second"])
+            for _, row in self.metadata.iloc[I[0]].iterrows()
+        ]
 
 
 DATASET_PATH: Final[str] = os.environ.get("DATASET_PATH", "data/dataset.parquet")
 SEARCHER: Final[SemanticSearcher] = SemanticSearcher(pd.read_parquet(DATASET_PATH))
 
 
+@dataclass
+class SearchResult:
+    id: str
+    second: int
+
+
 def get_video_url(video_id: str) -> str:
     return f"https://www.youtube.com/watch?v={video_id}"
+
+
+def display_search_results(results: list[SearchResult]) -> None:
+    col_count = 3  # Number of videos per row
+
+    col_num = 0  # Counter to keep track of the current column
+    row = st.empty()  # Placeholder for the current row
+
+    for result in results:
+        if col_num == 0:
+            row = st.columns(col_count)  # Create a new row of columns
+
+        with row[col_num]:
+            # Apply CSS styling to the video container
+            st.markdown(
+                """
+                <style>
+                .video-container {
+                    position: relative;
+                    padding-bottom: 56.25%;
+                    padding-top: 30px;
+                    height: 0;
+                    overflow: hidden;
+                }
+                .video-container iframe,
+                .video-container object,
+                .video-container embed {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                }
+                </style>
+                """,
+                unsafe_allow_html=True,
+            )
+
+            # Display the embedded YouTube video
+            st.video(get_video_url(result.id), start_time=result.second)
+
+        col_num += 1
+        if col_num >= col_count:
+            col_num = 0
 
 
 def main():
     st.set_page_config(page_title="video-semantic-search", layout="wide")
     st.header("Video Semantic Search")
 
-    lc, rc = st.columns(2)
-    lc.text_input("What are you looking for?", key="query")
+    st.text_input("What are you looking for?", key="query")
 
     query = st.session_state["query"]
     if query:
-        results = SEARCHER.search(query)
-        for i, row in results.iterrows():
-            url = get_video_url(row["id"])
-            rc.video(url, start_time=row["second"])
+        display_search_results(
+            SEARCHER.search(query)
+        )
 
 
 if __name__ == "__main__":
